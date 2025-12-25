c40f25c22d19d9a72110c2efade056dc7f01cceb
Akylai <loonarialight@gmail.com>
2025-12-25 20:48:18 +0600

    feat: animate earth appearance from top

---
+++ src/assets/assets.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import background from './background.png';
+import sun from './sun.png';
+import earth from './earth.png';
+import winter from './winter.png';
+import spring from './spring.png';
+import summer from './summer.png';
+import autumn from './autumn.png';
+
+const assets = {
+  background,
+  sun,
+  earth,
+  seasons: {
+    winter,
+    spring,
+    summer,
+    autumn,
+  },
+};
+
+export default assets;
